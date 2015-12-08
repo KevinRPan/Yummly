e80999f8659748e6c4e48c5613a06c0ed5cdab53
Kevin <kevipan@gmail.com>
2015-12-08 16:26:48 -0500

    roc not quite working

---
+++ roc.py
@@ -0,0 +1,210 @@
+"""
+=======================================
+Receiver Operating Characteristic (ROC)
+=======================================
+
+Example of Receiver Operating Characteristic (ROC) metric to evaluate
+classifier output quality.
+
+ROC curves typically feature true positive rate on the Y axis, and false
+positive rate on the X axis. This means that the top left corner of the plot is
+the "ideal" point - a false positive rate of zero, and a true positive rate of
+one. This is not very realistic, but it does mean that a larger area under the
+curve (AUC) is usually better.
+
+The "steepness" of ROC curves is also important, since it is ideal to maximize
+the true positive rate while minimizing the false positive rate.
+
+Multiclass settings
+-------------------
+
+ROC curves are typically used in binary classification to study the output of
+a classifier. In order to extend ROC curve and ROC area to multi-class
+or multi-label classification, it is necessary to binarize the output. One ROC
+curve can be drawn per label, but one can also draw a ROC curve by considering
+each element of the label indicator matrix as a binary prediction
+(micro-averaging).
+
+Another evaluation measure for multi-class classification is
+macro-averaging, which gives equal weight to the classification of each
+label.
+
+.. note::
+
+    See also :func:`sklearn.metrics.roc_auc_score`,
+             :ref:`example_model_selection_plot_roc_crossval.py`.
+
+"""
+print(__doc__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import cycle
+
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from scipy import interp
+import json 
+import numpy as np
+from pprint import pprint
+import os
+import logging
+from optparse import OptionParser
+import sys
+from time import time
+#import matplotlib.pyplot as plt
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.linear_model import RidgeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import NearestCentroid
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.utils.extmath import density
+from sklearn.linear_model import LogisticRegression
+from sklearn import grid_search
+from sklearn import metrics
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn import cross_validation
+from sklearn.cross_validation import KFold
+
+#path='/Box\ Sync/Kaggle/Yummly/'
+#os.chdir(path)
+
+with open('train.json') as data_file:
+  data=json.load(data_file)
+
+#input_dir = ''#'../input/'
+
+#with open(os.path.join(input_dir, 'train.json')) as train_f:
+#    data = json.loads(train_f.read())
+
+n=len(data)
+nsplit=n/2
+
+
+def normalize_input(X):
+    return (X.T / np.sum(X, axis=1)).T
+
+X=[x['ingredients'] for x in data]
+X= [dict(zip(x,np.ones(len(x)))) for x in X]
+
+vec = DictVectorizer()
+
+X= vec.fit_transform(X).toarray()
+X= normalize_input(X)
+X = X.astype(np.float32)
+
+feature_names = np.array(vec.feature_names_)
+
+lbl = LabelEncoder()
+
+y= [y['cuisine'] for y in data]
+y= lbl.fit_transform(y).astype(np.int32)
+
+label_names = lbl.classes_ 
+n_classes=len(label_names)
+#for i, l in enumerate(label_names):
+    #print('i: {}, l: {}'.format(i, l))
+
+X_train=X[0:nsplit]
+y_train=y[0:nsplit]
+X_test=X[nsplit:]
+y_test_pre=y[nsplit:]
+
+y_test=[[0]*n_classes]*len(y_test_pre)
+for i in range(len(y_test_pre)):
+    #print i,y_test[i],y_test_pre[i], y_test[i][y_test_pre[i]]
+    y_test[i]=[0]*n_classes
+    y_test[i][y_test_pre[i]]=1
+y_test=array(y_test)
+
+# Learn to predict each class against the other
+classifier = OneVsRestClassifier(LinearSVC())
+y_score = classifier.fit(X_train, y_train).decision_function(X_test)
+
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[i].T, y_score[:, i].T)
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+
+##############################################################################
+# Plot of a ROC curve for a specific class
+plt.figure()
+lw = 2
+plt.plot(fpr[2], tpr[2], color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic example')
+plt.legend(loc="lower right")
+plt.show()
+
+
+##############################################################################
+# Plot ROC curves for the multiclass problem
+
+# Compute macro-average ROC curve and ROC area
+
+# First aggregate all false positive rates
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+# Then interpolate all ROC curves at this points
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+# Finally average it and compute AUC
+mean_tpr /= n_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+# Plot all ROC curves
+plt.figure()
+plt.plot(fpr["micro"], tpr["micro"],
+         label='micro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["micro"]),
+         color='deeppink', linestyle=':', linewidth=4)
+
+plt.plot(fpr["macro"], tpr["macro"],
+         label='macro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["macro"]),
+         color='navy', linestyle=':', linewidth=4)
+
+colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+             label='ROC curve of class {0} (area = {1:0.2f})'
+             ''.format(i, roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Some extension of Receiver operating characteristic to multi-class')
+plt.legend(loc="lower right")
+plt.show()
