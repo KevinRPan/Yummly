4d055827c6ee84bf755ce75f8b0a44d44e245a75
Kevin <kevipan@gmail.com>
2015-12-09 03:13:34 -0500

    roc plot working

--- roc.py
+++ roc.py
@@ -138,7 +138,7 @@ fpr = dict()
 tpr = dict()
 roc_auc = dict()
 for i in range(n_classes):
-    fpr[i], tpr[i], _ = roc_curve(y_test[i].T, y_score[:, i].T)
+    fpr[i], tpr[i], _ = roc_curve(y_test[i], y_score[i, :])
     roc_auc[i] = auc(fpr[i], tpr[i])
 
 # Compute micro-average ROC curve and ROC area
